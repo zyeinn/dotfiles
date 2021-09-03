f8e981d6a27cf494ed9493d2e2fbf5ee35d71f04
Hélder Carneiro <60201403+zyeinn@users.noreply.github.com>
2021-09-03 17:12:35 +0100

    Remove ms

--- vpnpanel.sh
+++ vpnpanel.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 vpnip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
-lab=$(cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
 
 if [[ $vpnip == *"10."* ]]
 then
-   gwip=$(ip route | grep tun0 | grep via | cut -d " " -f 3)
-   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-4)
-   echo "VPN: $vpnip [$ping ms]"
+   echo "VPN: $vpnip"
 else
    echo "VPN: Disconnected"
 fi
